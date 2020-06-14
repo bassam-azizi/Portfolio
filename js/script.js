ffd6a8a729af2721990a45040588c5651dc83515
bassem <101010bassam@gmail.com>
2020-06-14 20:43:03 +0100

    make the arrows  fully appear when they should and disable theirs click events when they are disabled

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ const rightArrow = document.getElementById('rightArrow');
 const leftArrow = document.getElementById('leftArrow');
 
 let items = 0;
-let projectsTotalWidth = 0;
-let columns;
+// let projectsTotalWidth = 0;
+// let columns;
 
 const responsive = [
     { breakpoint: { width: 0, item: 1 } },
@@ -26,39 +26,50 @@ function load() {
     start();
 }
 let projectWidth;
-let slideWidth;
 function start() {
-    console.log(items);
     projectWidth = (project_slide_width / items) - margin;
     projects.forEach(element => {
         element.style.cssText = "width:" + projectWidth + "px; margin: " + (margin / 2) + "px";
-        projectsTotalWidth += projectWidth + margin;
     });
-    if (window.innerWidth <= 600) columns = Math.ceil((projectsTotalWidth / projectWidth));
-
-    if (window.innerWidth > 600) columns = Math.ceil((projectsTotalWidth / projectWidth)) / 2;
-
 }
 
 let counter = 0;
 function slide_right() {
-    slideWidth = columns * (projectWidth + (margin / 2));
 
-    if (counter >= (columns - items)) return;
+    if (project_slide.style.left !== "" || project_slide.style.left !== "0px") {
+        document.querySelector('.left').style.opacity = "1";
+        document.querySelector('.left').style.pointerEvents = "auto";
+    }
+    // MOVE THE SLIDE HORIZENTALY BY MULTIPLIYING THE PROJECTWIDTH WITH THE COUNTER AND GAVE IT4S OUTPUT AS A VALUE TO THE LEFT POSITION
     counter++;
-    // project_slide.style.marginLeft = ((-projectWidth * counter) - margin) + 'px';
-    project_slide.style.transform = 'translateX(' + (-(projectWidth + margin) * counter) + 'px)';
-
+    project_slide.style.left = -(projectWidth + margin) * counter + 'px';
+    // IF THE SLIDE HAVE NO MORE EXTRA THINGS TO SHOW? THAN WE BLOCK THE EVENT
+    if (project_slide.offsetWidth - parseInt(project_slide.style.left) + 1 >= project_slide.scrollWidth) {
+        document.querySelector('.right').style.opacity = "0.4";
+        document.querySelector('.right').style.pointerEvents = "none";
+    }
 }
-
 // rightArrow.addEventListener('click', slide_right, false);
 
 function slide_left() {
-    // if (project_slide.style.marginLeft === "") return;
-    // if (project_slide.style.marginLeft === "0px") return;
+    if (project_slide.style.left == "" || project_slide.style.left == "0px") {
+        return;
+    }
     counter--;
-    // project_slide.style.marginLeft = (-projectWidth * counter) + 'px';
-    project_slide.style.transform = 'translateX(' + (-(projectWidth + margin) * counter) + 'px)';
+    project_slide.style.left = (-(projectWidth + margin) * counter) + 'px';
+
+    if (project_slide.offsetWidth - parseInt(project_slide.style.left) + 1 < (project_slide.scrollWidth)) {
+        console.log('activate arrow right');
+
+        document.querySelector('.right').style.opacity = "1";
+        document.querySelector('.right').style.pointerEvents = "auto";
+    }
+    if (parseInt(project_slide.style.left) == 0) {
+        console.log('tgh');
+        document.querySelector('.left').style.opacity = ".4";
+        document.querySelector('.left').style.pointerEvents = "none";
+    }
+    // 
 }
 // leftArrow.addEventListener('click', slide_left, false);
 
